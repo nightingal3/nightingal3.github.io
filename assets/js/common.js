d85e57ad4cdae4e43be395d374ce71cabdb08fe4
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2026-08-28 13:47:00 +0000

    Give COLM a distinct venue color [ci skip]

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-    $('.links .abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-        $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
-    });
-    $('.links .bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-        $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
+$(document).ready(function(){$(".links .abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open"),$(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open")}),$(".links .bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open"),$(this).parent().parent().find(".abstract.hidden.open").toggleClass("open")}),$("a").removeClass("waves-effect waves-light")});
